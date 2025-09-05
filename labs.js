606235a0ab0322a0719c740268dec4ef9b1ba91c
BetterLordWilliam <willjordan05@icloud.com>
2025-09-05 15:10:55 -0700

    WO: Addressing module loading error.

--- labs.js
+++ labs.js
@@ -7,7 +7,6 @@ router.get('/0', (req, res) => {
     const lab0Index = `${serverRoot}/labs/0/index.html`;
 
     res.status(200);
-    res.set('Content-Type', 'text/html');
     res.sendFile(lab0Index);
 });
 
