13b934072af9aa77f9e6ec70196bfbdeb9a61cbc
BetterLordWilliam <willjordan05@icloud.com>
2025-09-11 20:18:59 -0700

    WO: Modified get all notes logic again, use a more idiomatic javascript way of doing it -- map method.

--- public/COMP4537/labs/1/js/StorageService.js
+++ public/COMP4537/labs/1/js/StorageService.js
@@ -15,15 +15,9 @@ export default class StorageService {
     }
 
     static getAllNotes() {
-        const notes = [];
-
-        for (let i = 0; i < window.localStorage.length; i++) {
-            const noteKey = window.localStorage.key(i);
-            const noteString = window.localStorage.getItem(noteKey);
-            const retrievedNote = new Note(noteKey, noteString);
-
-            notes.push(retrievedNote);
-        }
+        const notes = Object
+            .entries(window.localStorage)
+            .map(([key, value]) => new Note(key, value));
 
         return notes;
     }
