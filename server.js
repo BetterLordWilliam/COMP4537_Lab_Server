7d5e0981a7d202db46c342a245df38b21b1b898b
BetterLordWilliam <willjordan05@icloud.com>
2025-09-24 20:40:36 -0700

    WO: Rewrote the server to not use the expressJS library, since that is apparently a requirement.

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-import express from 'express';
+// import express from 'express';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
+// const app = express();
+
+// app.use(express.static('public'));
+
+// app.listen(port, () => {
+//     console.log(`Server running on ${port}`);
+// });
 
 const port = process.env.PORT || 3000;
-const app = express();
+const publicDirectory = path.normalize(path.resolve('./public'));
 
-app.use(express.static('public'));
+const contentTypes = {
+    '.html': 'text/html',
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.ico': 'image/x-icon'
+};
 
-app.listen(port, () => {
-    console.log(`Server running on ${port}`);
+const server = http.createServer((req, res) => {
+    let filePath = '';
+    let contentType = '';
+
+    // So we don't need to say index.html
+    if (req.url.endsWith('/')) {
+        filePath = path.join(publicDirectory, req.url, 'index.html');
+        contentType = contentTypes['.html'];
+    } else {
+        filePath = path.join(publicDirectory, req.url);
+        contentType = contentTypes[path.extname(req.url)] || 'application/octet-stream';
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('<h1>404 Not found</h1><p>Such a thing does not exist.</p>');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
 });
+
+server.listen(port, () => {
+    console.log(`server running on ${port}`);
+})
