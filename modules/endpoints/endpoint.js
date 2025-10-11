1edafe197b3aba96324e4b03a04d5fc6daef993a
BetterLordWilliam <willjordan05@icloud.com>
2025-10-11 14:31:30 -0700

    WO: Added access control allow origin headers for brysons website.

--- modules/endpoints/endpoint.js
+++ modules/endpoints/endpoint.js
@@ -10,17 +10,26 @@ export default class ApiEndpoint {
     }
 
     writeSuccess(res, data) {
-        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://blindy.net'
+        })
         res.end(JSON.stringify(data));
     }
 
     writeServerFail(res, data) {
-        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.writeHead(500, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://blindy.net'
+        });
         res.end(JSON.stringify(data));
     }
 
     writeBadRequest(res, data) {
-        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.writeHead(400, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://blindy.net'
+        });
         res.end(JSON.stringify(data));
     }
 }
